335770250efeca7a4e64c239e6cd9ed711b7c162
shar27 <shariq.ahmed731@gmail.com>
2021-11-24 13:15:56 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function MyApp({ Component, pageProps }) {
+  
+  import('bootstrap/dist/css/bootstrap.css');
+  
+  return <Component {...pageProps} />
+}
+
+export default MyApp
