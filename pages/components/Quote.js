c3918368b4062f3be532daf8d262d91c9579ce28
shar27 <shariq.ahmed731@gmail.com>
2021-11-28 15:11:03 +0100

    update class

--- pages/components/Quote.js
+++ pages/components/Quote.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Quote() {
     return (
 <div className="d-flex justify-content-center">
-<figure class="text-center">
-  <blockquote class="blockquote">
+<figure className="text-center">
+  <blockquote className="blockquote">
     <p>If you talk to a man in a language he understands, that goes to his head. 
     If you talk to him in his language, that goes to his heart.</p>
   </blockquote>
-  <figcaption class="blockquote-footer">
+  <figcaption className="blockquote-footer">
      <cite title="Source Title">Nelson Mandela</cite>
   </figcaption>
 </figure>
