3400c4aa09d0a25b1f12b14eab43a852a7590926
shar27 <shariq.ahmed731@gmail.com>
2021-11-30 15:32:13 +0100

    text

--- pages/components/Stats.js
+++ pages/components/Stats.js
@@ -9,28 +9,31 @@ function Stats() {
     return (
         <div className="bg-primary">
             <h1 className="fw-bold text-center me-4 text-white mb-4">My results</h1>
-            <div className="container">
+            <div className="container text-center">
                 <div className="row">
-                    <div className="col">
+                    <div className="col-sm-12 col-lg-4">
                     <h1 className="text-white fw-bold mt-4">1000+</h1>
-                <p className=" text-lead  text-white">Students that I have worked with.</p>
+                <p className=" text-lead fw-bold text-white">Students that I have worked with.</p>
                 <br/>
                     </div>
-                    <div className="col">
+                    <div className="col-sm-12 col-lg-4">
                 <h1 className="text-white fw-bold mt-4">100% </h1>
-                <p className=" text-lead  text-white">Have said they felt progress after a couple of weeks.</p>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
+                <p className=" text-lead fw-bold text-white">Have said they felt progress after a couple of weeks.</p>
+                
+                
                     </div>
-                    <div className="col">
+                    <div className="col-sm-12 col-lg-4">
                 <h1 className="text-white fw-bold mt-4">100%</h1>
-                <p className=" text-lead  text-white">Have referred me to others.</p>
+                <p className=" text-lead fw-bold text-white">Have referred me to others.</p>
                     </div>
                     
                 </div>
+                
             </div>
+            <br/>
+                <br/>
+                <br/>
+                <br/>
         </div>
            )
 }
