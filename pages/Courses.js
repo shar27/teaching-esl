a9fe2006bbfe5a9543ea0529359de8adeef9ccfc
shar27 <shariq.ahmed731@gmail.com>
2021-11-30 13:41:41 +0100

    alignment

--- pages/Courses.js
+++ pages/Courses.js
@@ -15,42 +15,42 @@ class Courses extends React.Component {
                 <Nav/>
                 <div className="container">
                     <div className="row">
-                        <div className="col-4 col-sm-12">
+                        <div className="col-4 col-sm-12 col-lg-4">
                         <h1>General English</h1>
                         <Link href="/Generaleng">
                         <a className="btn btn-primary">Read more</a>
                         </Link>
                         </div>
-                        <div className="col-4 col-sm-12">
+                        <div className="col-4 col-sm-12 col-lg-4">
                         <h1>Business English</h1>
                         <Link href="/Businesseng">
                             <a className="btn btn-primary">Read more</a>
                         </Link>
                         </div>
-                        <div className="col-4 col-sm-12">
+                        <div className="col-4 col-sm-12 col-lg-4">
                         <h1>Custom subject</h1>
-                        <br/>
+                        
                         <Link href="/Customeng">
                             <a className="btn btn-primary">Read more</a>
                         </Link>
                         </div>
-                        <div className="col-4 col-sm-12">
+                        <div className="col-4 col-sm-12 col-lg-4">
                         <h1>Grammar </h1>
-                        <br/>
+                        
                         <Link href="/Grammareng">
                             <a className="btn btn-primary">Read more</a>
                         </Link>
                         </div>
-                        <div className="col-4 col-sm-12">
+                        <div className="col-4 col-sm-12 col-lg-4">
                         <h1>IELTS</h1>
-                        <br/>
+                        
                         <Link href="/Ielts">
                             <a className="btn btn-primary">Read more</a>
                         </Link>
                         </div>
-                        <div className="col-4 col-sm-12">
-                        <h1>Beginners English</h1>
-                        <br/>
+                        <div className="col-4 col-sm-12 col-lg-4">
+                        <h1>Beginner English</h1>
+                        
                         <Link href="/Beginners">
                             <a className="btn btn-primary">Read more</a>
                         </Link>
