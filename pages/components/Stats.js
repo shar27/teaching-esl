32d8c2e7f16cb724303192d3f8daebdcb7cadf31
shar27 <shariq.ahmed731@gmail.com>
2021-12-02 15:21:02 +0100

    courses

--- pages/components/Stats.js
+++ pages/components/Stats.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {zoomIn} from 'react-animations'
 import CountUp from 'react-countup';
 
 import MailchimpSubscribe from "react-mailchimp-subscribe"
@@ -11,20 +11,20 @@ function Stats() {
             <h1 className="fw-bold text-center me-4 text-white mb-4">My results</h1>
             <div className="container text-center">
                 <div className="row">
-                    <div className="col-sm-12 col-lg-4">
-                    <h1 className="text-white fw-bold mt-4">1000+</h1>
-                <p className=" text-lead fw-bold text-white">Students that I have worked with.</p>
+                    <div className="col-sm-12 col-lg-4 bg-light rounded-top">
+                    <h1 className=" fw-bold mt-4">1000+</h1>
+                <p className=" text-lead fw-bold ">Students that I have worked with.</p>
                 <br/>
                     </div>
-                    <div className="col-sm-12 col-lg-4">
-                <h1 className="text-white fw-bold mt-4">100% </h1>
-                <p className=" text-lead fw-bold text-white">Have said they felt progress after a couple of weeks.</p>
+                    <div className="col-sm-12 col-lg-4 bg-light rounded-top">
+                <h1 className=" fw-bold mt-4">100% </h1>
+                <p className=" text-lead fw-bold ">Have said they felt progress after a couple of weeks.</p>
                 
                 
                     </div>
-                    <div className="col-sm-12 col-lg-4">
-                <h1 className="text-white fw-bold mt-4">100%</h1>
-                <p className=" text-lead fw-bold text-white">Have referred me to others.</p>
+                    <div className="col-sm-12 col-lg-4 bg-light rounded-top">
+                <h1 className=" fw-bold mt-4">100%</h1>
+                <p className=" text-lead fw-bold ">Have referred me to others.</p>
                     </div>
                     
                 </div>
