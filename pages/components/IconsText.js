61b191dda430875e359656329ff22e4ace67640b
shar27 <shariq.ahmed731@gmail.com>
2021-12-04 12:11:48 +0100

    new align

--- pages/components/IconsText.js
+++ pages/components/IconsText.js
@@ -5,7 +5,7 @@ function IconsText() {
         <div className="d-flex justify-content-evenly col-sm-12 col-lg-12 p-4">
             <h1 className="col-4 col-sm-12 col-lg-4 text-center">Learn anywhere</h1>
             <h1 className="col-4 col-sm-12 col-lg-4 text-center">Learn anytime</h1>
-            <h1 className=" col-4 ms-2 cl-lg-4 text-center">100% Satisfaction</h1>
+            <h1 className=" col-4 ms-2 cl-lg-4 text-center me-2">100% Satisfaction</h1>
         </div>
     )
 }
