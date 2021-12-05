7b2318fe711670f64c81b1da683f1978214c1d23
shar27 <shariq.ahmed731@gmail.com>
2021-12-05 13:15:46 +0100

    alignment

--- pages/components/IconsText.js
+++ pages/components/IconsText.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function IconsText() {
     return (
-        <div className="d-flex justify-content-evenly col-sm-12 col-lg-12 p-4">
-            <h1 className="col-4 col-sm-12 col-lg-4 text-center">Learn anywhere</h1>
-            <h1 className="col-4 col-sm-12 col-lg-4 text-center">Learn anytime</h1>
-            <h1 className=" col-4 ms-2 cl-lg-4 text-center me-2">100% Satisfaction</h1>
+        <div className="d-flex justify-content-evenly p-4">
+            <h1 className=" col-4 col-sm-4 col-md-4 col-lg-4 text-center ">Learn anywhere</h1>
+            <h1 className=" col-4 col-sm-4 col-md-4 col-lg-4 text-center ">Learn anytime</h1>
+            <h1 className=" col-4 col-sm-4 col-md-4 col-lg-4 text-center  ">100% Satisfaction</h1>
         </div>
     )
 }
