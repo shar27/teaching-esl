b6bf9a25e13a29172c8c9eb706d3503a4d5c105b
shar27 <shariq.ahmed731@gmail.com>
2021-12-18 20:21:24 +0100

    new

--- pages/Posts.js
+++ pages/Posts.js
@@ -8,7 +8,7 @@ export async function getStaticProps() {
 
   const client = createClient({
     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   })
 
 const res = await client.getEntries({content_type: 'posts'})
