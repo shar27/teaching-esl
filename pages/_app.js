e991e359d58ec5154a48ef59ec365a1c4f0229a3
shar27 <shariq.ahmed731@gmail.com>
2022-01-25 13:35:08 +0100

    adroll home page

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
  
+ 
+
+
   <Script 
    
    strategy="lazyOnload"
@@ -32,38 +35,7 @@ function MyApp({ Component, pageProps }) {
       
        <Googlead/>
 
-      <Script
-        dangerouslySetInnerHTML={{
-            __html: `
-        
-    adroll_adv_id = ${process.env.ADROLL_ID};
-    adroll_pix_id = ${process.env.ADROLL_PIX_ID};
-    adroll_version = "2.0";
-
-    (function(w, d, e, o, a) {
-        w.__adroll_loaded = true;
-        w.adroll = w.adroll || [];
-        w.adroll.f = [ 'setProperties', 'identify', 'track' ];
-        var roundtripUrl = "https://s.adroll.com/j/" + adroll_adv_id
-                + "/roundtrip.js";
-        for (a = 0; a < w.adroll.f.length; a++) {
-            w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) {
-                return function() {
-                    w.adroll.push([ n, arguments ])
-                }
-            })(w.adroll.f[a])
-        }
-
-        e = d.createElement('script');
-        o = d.getElementsByTagName('script')[0];
-        e.async = 1;
-        e.src = roundtripUrl;
-        o.parentNode.insertBefore(e, o);
-    })(window, document);
-    adroll.track("pageView");
-`
-        }}
-    />    
+      
 
       
        <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,12 @@ import useTranslation from "next-translate/useTranslation";
 import Faqs from '../components/components/Faqs'
 
 export default function Home() {
+  
+  
+  
+  
+  
+  
   const hoveredStyle = {
     cursor: "pointer",
   };
@@ -44,6 +50,41 @@ export default function Home() {
           content="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJlbmdsaXNoc3R1ZmYuY29tIiwiZXhwIjoxNjQzNTg3MjAwfQ.L4TSKTS7KKLqznNct-cwNWccrOvaxAK-meXchtgWmVc"
         ></meta>
         <link rel="icon" href="https:teacher-esl.com" />
+
+        <Script
+        strategy='lazyOnload'
+        dangerouslySetInnerHTML={{
+            __html: `
+        
+    adroll_adv_id = ${process.env.ADROLL_ID};
+    adroll_pix_id = ${process.env.ADROLL_PIX_ID};
+    adroll_version = "2.0";
+
+    (function(w, d, e, o, a) {
+        w.__adroll_loaded = true;
+        w.adroll = w.adroll || [];
+        w.adroll.f = [ 'setProperties', 'identify', 'track' ];
+        var roundtripUrl = "https://s.adroll.com/j/" + adroll_adv_id
+                + "/roundtrip.js";
+        for (a = 0; a < w.adroll.f.length; a++) {
+            w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) {
+                return function() {
+                    w.adroll.push([ n, arguments ])
+                }
+            })(w.adroll.f[a])
+        }
+
+        e = d.createElement('script');
+        o = d.getElementsByTagName('script')[0];
+        e.async = 1;
+        e.src = roundtripUrl;
+        o.parentNode.insertBefore(e, o);
+    })(window, document);
+    adroll.track("pageView");
+`
+        }}
+    />    
+
       </Head>
       <div className="bg-none">
         <Logo />
